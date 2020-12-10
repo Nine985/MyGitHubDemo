85a9a56fd908adb08669990cb64f8fe1a9bfafb1
zst <2944275384@qq.com>
2020-12-10 17:05:15 +0800

    啊哈哈哈哈

--- app/src/main/java/com/example/mygithubdemo/MainActivity.java
+++ app/src/main/java/com/example/mygithubdemo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a="代码";
-        String b="郝东海变了!";
+
     }
 }
